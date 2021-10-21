69e532e7d67a7d79cbe5f20924f69cc790214535
retno sonjaya <beno.sons@khansia.co.id>
2021-10-21 20:59:54 +0700

    update

--- public/action-js/global-admin.js
+++ public/action-js/global-admin.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
     }
     Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
   }
+
+  $('.nav-item').on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('active');
+    checkarrow(this);
+  })
 });
+
+function checkarrow(ini){
+  if($(ini).hasClass('active')){
+      $(ini).children().children().find('.icon-nya').text('expand_more');
+      $(ini).find('.submenu').css('display', 'block');
+      $(ini).find('.active-bang').addClass('bg-gradient-primary');
+    }else{
+      $(ini).children().children().find('.icon-nya').text('expand_less');
+      $(ini).find('.submenu').css('display', 'none');
+      $(ini).find('.active-bang').removeClass('bg-gradient-primary');
+    }
+}
